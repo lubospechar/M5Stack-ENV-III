ef95b52f720876d8761cc4634538fadff03d6389
Luboš Pechar <lubos.pechar@prirodou.cz>
2025-10-31 22:20:08 +0100

    Update `__init__.py` to include QMP6988 sensor and related exceptions in the public API

--- src/m5stack/__init__.py
+++ src/m5stack/__init__.py
@@ -1,10 +1,13 @@
 """Public API for the M5Stack sensor library."""
 
-from .exceptions import SHT30Error, SensorError
+from .exceptions import SensorError, SHT30Error, QMP6988Error
 from .sht30 import SHT30
+from .qmp6988 import QMP6988
 
 __all__ = [
     "SHT30",
     "SHT30Error",
+    "QMP6988",
+    "QMP6988Error",
     "SensorError",
 ]
